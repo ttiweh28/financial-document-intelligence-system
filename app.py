22704ff339104cd75f8f9d94a7c4decea5ed0c95
Ahmed Ibrahim <ahmedm3377@yahoo.com>
2026-04-09 23:59:09 -0500

    add endpoints

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI, status
+from agents import Runner
+from fastapi import FastAPI, File, Form, UploadFile, status
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from openai import OpenAI
+from main import orchestrator_agent, get_session
+from dotenv import load_dotenv
+load_dotenv()
 
 
 app = FastAPI(title="Financial AI assistant")
@@ -14,6 +19,34 @@ app.add_middleware(
 @app.get("/health", status_code=status.HTTP_200_OK)
 async def health_check():
     return {"status": "healthy"}
+
+
+@app.post("/process-document")
+async def process_document(user_id: str = Form(...), file: UploadFile = File(...)):
+
+
+    client = OpenAI()
+    vector_store = client.vector_stores.create(
+        name=f"{user_id}_vector_store",
+    )
+
+    client.vector_stores.files.upload_and_poll(
+        vector_store_id=vector_store.id,
+        file=file.file,
+    )
+
+    return {
+        "message": f"File uploaded and processing started for user_id: {user_id}",
+        "vector_store_id": vector_store.id,
+        "filename": file.filename,
+    }
+
+
+@app.post("/follow_up")
+async def follow_up(user_id: str, question: str):
+    session = get_session(user_id)
+    result = await Runner.run(orchestrator_agent, session=session, input=question)
+    return result.final_output
 
 
 if __name__ == "__main__":
